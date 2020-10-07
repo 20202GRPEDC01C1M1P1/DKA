249cd8357c4cf7f869f6565be6623c4d996d433f
Thiago Vieira de Aguiar <thiagoaguiar@tic.ufrj.br>
2020-10-07 09:10:04 -0300

    Desafio 1 Desafio 2

--- app/src/main/java/br/pro/aguiar/dka/MainActivity.kt
+++ app/src/main/java/br/pro/aguiar/dka/MainActivity.kt
@@ -1,18 +1,10 @@
 package br.pro.aguiar.dka
 
 import android.content.Context
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import androidx.annotation.RequiresApi
-import br.pro.aguiar.dka.async.Repository
-import br.pro.aguiar.dka.async.UserAsyncTask
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.BufferedReader
 import java.io.File
-import java.io.FileReader
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,40 +13,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        openFileOutput("exemplo.txt", Context.MODE_PRIVATE).use {
-            it.write("Thiago Vieira de Aguiar".toByteArray())
-            it.write("\nthiago.aguiar@prof.infnet.edu.br".toByteArray())
-            it.write("\n(21) 995344540".toByteArray())
-            it.write("\nInfnet".toByteArray())
+        // editFileName
+        // btnValidateFile
+        // textViewFileExiste
+        // textViewFileLegivel
+        // editTextTextMultiLine
+        // btnSalveFile
+        // File
+
+        btnValidateFile.setOnClickListener {
+            var nomeArquivo = editFileName.text.toString()
+            var file = File(filesDir, nomeArquivo)
+
+            if (file.exists()){
+                textViewFileExiste.text = "Sim"
+            } else
+                textViewFileExiste.text = "Não"
+
+            if (file.canRead())
+                textViewFileLegivel.text = "Sim"
+            else
+                textViewFileLegivel.text = "Não"
+
+            if (file.canWrite())
+                enableComponents(true)
+            else
+                enableComponents(false)
         }
 
-
-        var file = File(filesDir, "exemplo.txt")
-        if (file.exists()){
-
-            var fileReader = FileReader(file)
-
-            var bufferedReader = BufferedReader(fileReader)
-
-            bufferedReader.readLines().forEach {
-                textViewMain.text = textViewMain.text.toString() + "\n" + it
+        btnSalveFile.setOnClickListener {
+            var conteudo = editTextTextMultiLine.text.toString()
+            var arquivo = editFileName.text.toString()
+            openFileOutput(arquivo, Context.MODE_PRIVATE).use {
+                it.write(conteudo.toByteArray())
             }
         }
+    }
 
-        // Escrever em arquivo
-//        openFileOutput("exemplo.txt", Context.MODE_PRIVATE).use {
-//            it.write("Thiago Vieira de Aguiar".toByteArray())
-//        }
-
-//        var file = File(filesDir, "exemplo2.txt")
-//        var fileWriter = FileWriter(file)
-//        var bufferedWriter = BufferedWriter(fileWriter)
-//        bufferedWriter.write("Thiago Vieira de Aguiar")
-//
-//        if (file.exists()){
-//            textViewMain.text = "Arquivo existe"
-//        } else {
-//            textViewMain.text = "Arquivo não existe"
-//        }
+    fun enableComponents(bool: Boolean){
+        editTextTextMultiLine.isEnabled = bool
+        btnSalveFile.isEnabled = bool
     }
 }
