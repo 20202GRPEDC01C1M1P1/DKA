d2190c32d22f8bf63e56262fb7e3a63308010da2
Thiago Vieira de Aguiar <thiagoaguiar@tic.ufrj.br>
2020-10-07 08:16:10 -0300

    BufferedWriter

--- app/src/main/java/br/pro/aguiar/dka/MainActivity.kt
+++ app/src/main/java/br/pro/aguiar/dka/MainActivity.kt
@@ -3,16 +3,30 @@ package br.pro.aguiar.dka
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import br.pro.aguiar.dka.async.Repository
 import br.pro.aguiar.dka.async.UserAsyncTask
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Repository().execute("Termo de Busca")
-        UserAsyncTask().execute(1)
-        Log.i("AsyncTask", "MainThread")
+
+
+        var file = File(filesDir, "exemplo2.txt")
+        var fileWriter = FileWriter(file)
+        var bufferedWriter = BufferedWriter(fileWriter)
+        bufferedWriter.write("Thiago Vieira de Aguiar")
+
+        if (file.exists()){
+            textViewMain.text = "Arquivo existe"
+        } else {
+            textViewMain.text = "Arquivo não existe"
+        }
     }
 }
