7e832d46159075317c46ec7fc58cb505a1a0e733
Thiago Vieira de Aguiar <thiagoaguiar@tic.ufrj.br>
2020-10-07 08:38:54 -0300

    BufferedReader

--- app/src/main/java/br/pro/aguiar/dka/MainActivity.kt
+++ app/src/main/java/br/pro/aguiar/dka/MainActivity.kt
@@ -1,25 +1,50 @@
 package br.pro.aguiar.dka
 
 import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import br.pro.aguiar.dka.async.Repository
 import br.pro.aguiar.dka.async.UserAsyncTask
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.BufferedWriter
+import java.io.BufferedReader
 import java.io.File
-import java.io.FileWriter
+import java.io.FileReader
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         openFileOutput("exemplo.txt", Context.MODE_PRIVATE).use {
             it.write("Thiago Vieira de Aguiar".toByteArray())
+            it.write("\nthiago.aguiar@prof.infnet.edu.br".toByteArray())
+            it.write("\n(21) 995344540".toByteArray())
+            it.write("\nInfnet".toByteArray())
         }
+
+
+        var file = File(filesDir, "exemplo.txt")
+        if (file.exists()){
+
+            var fileReader = FileReader(file)
+
+            var bufferedReader = BufferedReader(fileReader)
+
+            bufferedReader.readLines().forEach {
+                textViewMain.text = textViewMain.text.toString() + "\n" + it
+            }
+        }
+
+        // Escrever em arquivo
+//        openFileOutput("exemplo.txt", Context.MODE_PRIVATE).use {
+//            it.write("Thiago Vieira de Aguiar".toByteArray())
+//        }
 
 //        var file = File(filesDir, "exemplo2.txt")
 //        var fileWriter = FileWriter(file)
