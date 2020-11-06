711d355bbf6c7373167199d662067d2167af703a
Thiago Vieira de Aguiar <thiagoaguiar@tic.ufrj.br>
2020-11-06 08:40:08 -0300

    Exemplo Rede Social: Informações completas dos Posts com SnapshotListener.

--- app/src/main/java/br/pro/aguiar/dka/social/ui/info/InfoPostFragment.kt
+++ app/src/main/java/br/pro/aguiar/dka/social/ui/info/InfoPostFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import br.pro.aguiar.dka.R
 import br.pro.aguiar.dka.social.model.Post
 import br.pro.aguiar.dka.social.viewmodel.SocialViewModel
@@ -15,7 +16,9 @@ import kotlinx.android.synthetic.main.fragment_info_post.*
 class InfoPostFragment : Fragment() {
 
     private lateinit var socialViewModel: SocialViewModel
+    private lateinit var infoPostViewModel: InfoPostViewModel
     private lateinit var socialViewModelFactory: SocialViewModelFactory
+    private lateinit var infoPostViewModelFactory: InfoPostViewModelFactory
     private var post: Post? = null
 
     override fun onCreateView(
@@ -34,12 +37,35 @@ class InfoPostFragment : Fragment() {
             socialViewModel = ViewModelProvider(it, socialViewModelFactory)
                 .get(SocialViewModel::class.java)
             post = socialViewModel.post
-
-            textVIewInfoPostTitulo.text = post?.titulo
-            textVIewInfoPostAutor.text = post?.autor
-            textViewInfoPostConteudo.text = post?.conteudo
-            textVIewInfoPostQuantidadeCurtida.text = post?.curtidas.toString()
         }
 
+        infoPostViewModelFactory = InfoPostViewModelFactory()
+        infoPostViewModel = ViewModelProvider(this, infoPostViewModelFactory)
+            .get(InfoPostViewModel::class.java)
+
+        if (post != null && post!!.id != null){
+            infoPostViewModel.get(post!!.id!!)
+                .addSnapshotListener { snapshot, error ->
+                    if (error != null){
+                        // emitir um erro
+                    } else if (snapshot != null && snapshot.exists()) {
+                        var post = snapshot.toObject(Post::class.java)
+                        if (post != null)
+                            exibirDadosNoLayut(post!!)
+                    }
+                }
+        } else
+            findNavController().popBackStack()
+
+        btnCurtirPost.setOnClickListener {
+            infoPostViewModel.curtir(post!!)
+        }
+    }
+
+    private fun exibirDadosNoLayut(post: Post) {
+        textVIewInfoPostTitulo.text = post?.titulo
+        textVIewInfoPostAutor.text = post?.autor
+        textViewInfoPostConteudo.text = post?.conteudo
+        textVIewInfoPostQuantidadeCurtida.text = post?.curtidas.toString()
     }
 }
--- app/src/main/java/br/pro/aguiar/dka/social/ui/info/InfoPostViewModel.kt
+++ app/src/main/java/br/pro/aguiar/dka/social/ui/info/InfoPostViewModel.kt
@@ -1,7 +1,36 @@
 package br.pro.aguiar.dka.social.ui.info
 
 import androidx.lifecycle.ViewModel
+import br.pro.aguiar.dka.social.model.Post
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
 
 class InfoPostViewModel: ViewModel() {
-    // fun curtir()
+
+    var db = FirebaseFirestore.getInstance()
+    var collection = db.collection("posts")
+
+    //collection.get()
+    //collection.addSnapshotListener { ... }
+
+    //collection.document(..).get()
+    //collection.document(..).addSnapshotListener { ... }
+
+    fun curtir(post: Post) {
+        var document = collection.document(post.id!!)
+        document.set(
+            hashMapOf(
+                "curtidas" to post.curtidas++
+            ),
+            SetOptions.merge()
+        )
+    }
+
+    fun get(id: String): DocumentReference {
+        var document = collection.document(id)
+        return document
+    }
 }
